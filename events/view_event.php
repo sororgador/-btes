12d0a6e4723f64975978aa7a4a62b068c439161e
AlaaAdel810 <a.alsfagsi@uot.edu.ly>
2024-12-13 20:43:54 +0200

    Update view_event.php

--- events/view_event.php
+++ events/view_event.php
@@ -1,9 +1,28 @@
+<?php
+ob_start(); // بدء التخزين المؤقت
+
+include_once("connection.php");
+include_once("class_event.php");
+
+if (isset($_POST['search'])) {
+    $event->search($conn);
+} else if (isset($_POST['booking'])) {
+    header('Location: booking.php'); // استخدم "Location" بحروف كبيرة
+    exit; // تأكد من الخروج بعد استخدام header
+} else {
+    $event->displayformsearch($conn);
+}
+
+echo $event->displayEvent($conn);
+?>
+
 <html>
 <body>
 <div class="form-container">
     <form class="form" method="post">
         <button type="submit" name="add" class="btn btn-primary btn-large">Add review</button>
         <button type="submit" name="fav" class="btn btn-secondary btn-large">قائمة المفضلة</button>
+     <button type="submit" name="booked" class="btn btn-secondary btn-large">الغاء حجز</button>
     </form>
 </div>
 </body>
@@ -16,6 +35,10 @@ if (isset($_POST['add'])) {
 }
 if (isset($_POST['fav'])) {
     header('Location: login_fav.php'); // تأكد من عدم وجود ".php" مرتين
+    exit; // تأكد من الخروج بعد استخدام header
+}
+if (isset($_POST['booked'])) {
+    header('Location: delete_book.php'); // تأكد من عدم وجود ".php" مرتين
     exit; // تأكد من الخروج بعد استخدام header
 }
 
